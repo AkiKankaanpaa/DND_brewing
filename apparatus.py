8e3937f81be73855375b0fc1c1a99047b92d82f6
Kiriela <kiriela@users.noreply.github.com>
2020-08-31 04:06:58 +0300

    Improved on the design, added a better solution to the dropdown-menus, changed the layout, added recognition whether or not a plant is erranous or not

--- apparatus.py
+++ apparatus.py
@@ -5,8 +5,9 @@ import string
 
 # Used to determine the time units
 TIME_UNITS = {"0": 0, "Rounds": 1, "Minutes": 2, "Hours": 3, "Days": 4, "Months": 5, "Infinite": 6}
+
 BIOMES = ["All", "Arctic", "Coastal", "Deserts", "Forests", "Hills", "Jungles", "Lakes", "Mountain Peaks",
-          "Plains", "Ocean", "Swamps", "Volcanic" ]
+          "Plains", "Ocean", "Swamps", "Volcanic"]
 RARITIES = ["Common", "Uncommon", "Rare", "Very Rare", "Myth"]
 PORTIONS = [1, 2, 3, 4]
 
@@ -15,60 +16,20 @@ class Window:
     def __init__(self):
         self.__window = Tk()
         self.__window.title("Brewing Apparatus")
+        self.__window.configure(bg='dark goldenrod')
 
-        self.__form = Label(text="", fg="black", bg="powder blue", height=1, width=100)
-        self.__damage = Label(text="", fg="black", bg="sky blue", height=1, width=100)
-        self.__dc = Label(text="", fg="black", bg="powder blue", height=1, width=100)
-        self.__time = Label(text="", fg="black", bg="sky blue", height=1, width=100)
-        self.__effects = Label(text="", fg="black", bg="powder blue", height=1, width=100)
-        self.__attributes = Label(text="", fg="black", bg="sky blue", height=1, width=100)
-        self.__crafting_dc = Label(text="", fg="black", bg="powder blue", height=1, width=100)
+        # Used in calculation of the final crafting DC
+        self.__dc_calc = {"Die": 0, "Plant Amount": 0, "Attribute Amount": 0, "Effect Amount": 0}
 
-        self.__biome_variable = StringVar()
-        self.__biome_variable.set(BIOMES[0])
-        self.__biome_option = OptionMenu(self.__window, self.__biome_variable, *BIOMES)
-
-        self.__rarity_variable = StringVar()
-        self.__rarity_variable.set(RARITIES[0])
-        self.__rarity_option = OptionMenu(self.__window, self.__rarity_variable, *RARITIES)
-
-        self.__shameless_self_promotionlabel = Label(text="Brought to you by an underpaid intern",
-                                                     fg="black", bg="sky blue", height=1, width=42)
-
-        self.__shameless_self_promotionlabel2 = Label(text="Version 1.3 Alpha (Form Additions)",
-                                                     fg="black", bg="powder blue", height=1, width=42)
-
-        self.__shameless_self_promotionlabel3 = Label(text="This is not a finalized version of the product.",
-                                                     fg="black", bg="teal", height=1, width=100)
-
-        self.__errorlabel = Label(text="", fg="black", bg="sky blue", height=1, width=100)
-
-        self.__formvalue = IntVar()
-
-        self.__random_attribute = Label(text="", fg="black", bg="sky blue", height=1, width=42)
-        self.__random_effect = Label(text="", fg="black", bg="sky blue", height=1, width=42)
-        self.__random_plant = Label(text="", fg="black", bg="sky blue", height=1, width=42)
-
-        self.__formcheck = Checkbutton(text="Show Suitable Bases", variable=self.__formvalue, onvalue=1, offvalue=0)\
-            .grid(column=2, row=0, sticky=W)
-
-        self.__e1 = Entry(self.__window, width=50, fg="black", bg="light grey")
-        self.__e1.grid(row=0, column=0)
-
-        self.__e2 = Entry(self.__window, width=50, fg="black", bg="light grey")
-        self.__e2.grid(row=1, column=0)
-
-        self.__e3 = Entry(self.__window, width=50, fg="black", bg="light grey")
-        self.__e3.grid(row=2, column=0)
-
-        self.__e4 = Entry(self.__window, width=50, fg="black", bg="light grey")
-        self.__e4.grid(row=3, column=0)
-
-        self.__e5 = Entry(self.__window, width=50, fg="black", bg="light grey")
-        self.__e5.grid(row=4, column=0)
-
-        self.__e6 = Entry(self.__window, width=50, fg="black", bg="light grey")
-        self.__e6.grid(row=5, column=0)
+        # Information labels for the created potion
+        self.__form = Label(text="", fg="black", bg="olive drab", height=1, width=100)
+        self.__damage = Label(text="", fg="black", bg="dark olive green", height=1, width=100)
+        self.__dc = Label(text="", fg="black", bg="olive drab", height=1, width=100)
+        self.__time = Label(text="", fg="black", bg="dark olive green", height=1, width=100)
+        self.__effects = Message(text="", fg="black", bg="olive drab", aspect=1500, justify=CENTER)
+        self.__attributes = Message(text="", fg="black", bg="dark olive green", aspect=1500, justify=CENTER)
+        self.__crafting_dc = Label(text="", fg="black", bg="olive drab", height=1, width=100)
+        self.__errorlabel = Label(text="", fg="black", bg="dark olive green", height=1, width=100)
 
         self.__form.grid(row=0, column=1, sticky=N + W + E + S)
         self.__damage.grid(row=1, column=1, sticky=N + W + E + S)
@@ -79,33 +40,71 @@ class Window:
         self.__crafting_dc.grid(row=6, column=1, sticky=N + W + E + S)
         self.__errorlabel.grid(row=7, column=1, sticky=N + W + E + S)
 
-        self.__shameless_self_promotionlabel.grid(row=6, column=0, sticky=N + W + E + S)
-        self.__shameless_self_promotionlabel2.grid(row=7, column=0, sticky=N + W + E + S)
-        self.__shameless_self_promotionlabel3.grid(row=8, column=1, sticky=N + W + E + S)
+        # Clickdown-menus for the biome and rarity
+        self.__biome_variable = StringVar()
+        self.__biome_variable.set(BIOMES[0])
+        self.__biome_option = OptionMenu(self.__window, self.__biome_variable, *BIOMES)
+        self.__biome_option.configure(height=1, bg='dark goldenrod', width=15)
+        self.__biome_option.grid(row=0, column=3, sticky=N + W + E + S, columnspan=1)
 
-        self.__random_effect.grid(row=6, column=2, sticky=N + W + E + S)
-        self.__random_attribute.grid(row=8, column=2, sticky=N + W + E + S)
-        self.__random_plant.grid(row=4, column=2, sticky=N + W + E + S)
+        self.__rarity_variable = StringVar()
+        self.__rarity_variable.set(RARITIES[0])
+        self.__rarity_option = OptionMenu(self.__window, self.__rarity_variable, *RARITIES)
+        self.__rarity_option.configure(height=1, bg='dark goldenrod', width=15)
+        self.__rarity_option.grid(row=0, column=4, sticky=N + W + E + S, columnspan=1)
 
-        self.__biome_option.grid(row=1, column=2, sticky=N + W + E + S)
-        self.__rarity_option.grid(row=2, column=2, sticky=N + W + E + S)
+        self.__shameless_self_promotionlabel = Label(text="This is not a finalized version of the product.",
+                                                     fg="black", bg="chartreuse4", height=1, width=100)
+        self.__shameless_self_promotionlabel.grid(row=8, column=1, sticky=N + W + E + S)
 
-        Button(self.__window, text="Create Potion", command=self.press_potion, height=1, width=42,
-               fg="black", bg="teal").grid(row=8, column=0, sticky=N)
+        self.__random_plant = Label(text="", fg="black", bg="dark olive green", height=1, width=21)
+        self.__random_effect = Label(text="", fg="black", bg="dark olive green", height=1, width=21)
+        self.__random_attribute = Label(text="", fg="black", bg="dark olive green", height=1, width=21)
 
-        Button(self.__window, text="Random Effect", command=self.assign_random_effect, height=1, width=42,
-               fg="black", bg="teal").grid(row=5, column=2, sticky=N)
+        self.__random_plant.grid(row=2, column=3, columnspan=2, sticky=N + W + E + S)
+        self.__random_effect.grid(row=4, column=3, columnspan=2, sticky=N + W + E + S)
+        self.__random_attribute.grid(row=6, column=3, columnspan=2, sticky=N + W + E + S)
 
-        Button(self.__window, text="Random Attribute", command=self.assign_random_attribute, height=1, width=42,
-               fg="black", bg="teal").grid(row=7, column=2, sticky=N)
+        # Choose to either show the desired bases or not
+        self.__formvalue = IntVar()
+        self.__formcheck = Checkbutton(text="Show Suitable Bases", bg='dark goldenrod',
+                                       variable=self.__formvalue, onvalue=1, offvalue=0)
+        self.__formcheck.grid(column=0, columnspan=1, row=0, sticky=W)
 
-        Button(self.__window, text="Random Plant", command=self.assign_random_plant, height=1, width=42,
-               fg="black", bg="teal").grid(row=3, column=2, sticky=N)
+        # Entry-windows for the user to enter the plant names
+        self.__entry1 = Entry(self.__window, width=50, fg="black", bg="lemon chiffon")
+        self.__entry2 = Entry(self.__window, width=50, fg="black", bg="lemon chiffon")
+        self.__entry3 = Entry(self.__window, width=50, fg="black", bg="lemon chiffon")
+        self.__entry4 = Entry(self.__window, width=50, fg="black", bg="lemon chiffon")
+        self.__entry5 = Entry(self.__window, width=50, fg="black", bg="lemon chiffon")
+        self.__entry6 = Entry(self.__window, width=50, fg="black", bg="lemon chiffon")
 
-        # Used in calculation of the final crafting DC
-        self.__dc_calc = {"Die": 0, "Plant Amount": 0, "Attribute Amount": 0, "Effect Amount": 0}
+        self.__entry1.grid(row=1, column=0)
+        self.__entry2.grid(row=2, column=0)
+        self.__entry3.grid(row=3, column=0)
+        self.__entry4.grid(row=4, column=0)
+        self.__entry5.grid(row=5, column=0)
+        self.__entry6.grid(row=6, column=0)
+
+        self.__defined_button = Button(self.__window, text="Create Potion", command=self.press_potion,
+                                       height=1, width=42, fg="black", bg="chartreuse4")
+
+        self.__effect_button = Button(self.__window, text="Random Effect", command=self.assign_random_effect,
+                                      height=1, width=42, fg="black", bg="chartreuse4")
+
+        self.__attribute_button = Button(self.__window, text="Random Attribute", command=self.assign_random_attribute,
+                                         height=1, width=42, fg="black", bg="chartreuse4")
+
+        self.__plant_button = Button(self.__window, text="Random Plant", command=self.assign_random_plant,
+                                     height=1, width=42, fg="black", bg="chartreuse4")
+
+        self.__defined_button.grid(row=8, column=0, sticky=N + W + E + S)
+        self.__effect_button.grid(row=3, column=3, columnspan=2, sticky=N + W + E + S)
+        self.__attribute_button.grid(row=5, column=3, columnspan=2, sticky=N + W + E + S)
+        self.__plant_button.grid(row=1, column=3, columnspan=2, sticky=N + W + E + S)
 
         # Reads the plants.json file to generate the data structure for the plants
+        # does the same with effect and attributes as well
         with open("plants.json", "r") as plant_json:
             self.__PLANTS = json.load(plant_json)
 
@@ -120,59 +119,87 @@ class Window:
         plant_amount = 0
         plantlist = []
 
-        if self.__e1.get() != "":
-            namelist.append(self.__e1.get().lower())
+        # Retrieving the strings from the Entry-windows
+        if self.__entry1.get() != "":
+            namelist.append(self.__entry1.get().lower())
+            plant_amount += 1
+        if self.__entry2.get() != "":
+            namelist.append(self.__entry2.get().lower())
+            plant_amount += 1
+        if self.__entry3.get() != "":
+            namelist.append(self.__entry3.get().lower())
+            plant_amount += 1
+        if self.__entry4.get() != "":
+            namelist.append(self.__entry4.get().lower())
+            plant_amount += 1
+        if self.__entry5.get() != "":
+            namelist.append(self.__entry5.get().lower())
+            plant_amount += 1
+        if self.__entry6.get() != "":
+            namelist.append(self.__entry6.get().lower())
             plant_amount += 1
 
-        if self.__e2.get() != "":
-            namelist.append(self.__e2.get().lower())
-            plant_amount += 1
-
-        if self.__e3.get() != "":
-            namelist.append(self.__e3.get().lower())
-            plant_amount += 1
-
-        if self.__e4.get() != "":
-            namelist.append(self.__e4.get().lower())
-            plant_amount += 1
-
-        if self.__e5.get() != "":
-            namelist.append(self.__e5.get().lower())
-            plant_amount += 1
-
-        if self.__e6.get() != "":
-            namelist.append(self.__e6.get().lower())
-            plant_amount += 1
-
+        # Checking if the plant-name given exists within the database
+        counter = 1
         for name in namelist:
+            plant_has_not_been_found = True
             for plant in self.__PLANTS:
                 if plant["Name"] == name:
                     plantlist.append(plant)
+                    self.color_entry(counter, True)
+                    plant_has_not_been_found = False
+                    break
+            if plant_has_not_been_found:
+                self.color_entry(counter, False)
+            counter += 1
 
+        # Checking if the given plant amount is the same as the verified amount
         if len(plantlist) == len(namelist):
-            self.__errorlabel.configure(text="", fg="black", bg="sky blue")
-            self.__dc_calc["Plant Amount"] = plant_amount
-            self.calculate_save(plantlist)
-            self.calculate_damage(plantlist)
-            self.calculate_time(plantlist)
-            self.determine_effects(plantlist)
-            self.determine_attributes(plantlist)
-            self.calculate_dc()
+            self.make_potion(plantlist, plant_amount)
 
-            if self.__formvalue.get() == 1:
-                self.determine_form(plantlist)
-            else:
-                self.__form.configure(text="")
-
+        # If it isnt, show an error to the user
         else:
-            self.__damage.configure(text="")
-            self.__dc.configure(text="")
-            self.__time.configure(text="")
-            self.__effects.configure(text="")
-            self.__attributes.configure(text="")
-            self.__crafting_dc.configure(text="")
+            self.make_potion_error()
+
+    def make_potion(self, plantlist, plant_amount):
+        self.__errorlabel.configure(text="", fg="black", bg="dark olive green")
+        self.__dc_calc["Plant Amount"] = plant_amount
+        self.calculate_save(plantlist)
+        self.calculate_damage(plantlist)
+        self.calculate_time(plantlist)
+        self.determine_effects(plantlist)
+        self.determine_attributes(plantlist)
+        self.calculate_dc()
+
+        if self.__formvalue.get() == 1:
+            self.determine_form(plantlist)
+        else:
             self.__form.configure(text="")
-            self.__errorlabel.configure(text="Unrecognized Ingredients or No Plants Entered.", fg="black", bg="red")
+
+    def make_potion_error(self):
+        self.__damage.configure(text="")
+        self.__dc.configure(text="")
+        self.__time.configure(text="")
+        self.__effects.configure(text="")
+        self.__attributes.configure(text="")
+        self.__crafting_dc.configure(text="")
+        self.__form.configure(text="")
+        self.__errorlabel.configure(text="Unrecognized Ingredients or No Plants Entered.",
+                                    fg="black", bg="firebrick")
+
+    def color_entry(self, counter, plant_found):
+        entries = {
+            1: self.__entry1,
+            2: self.__entry2,
+            3: self.__entry3,
+            4: self.__entry4,
+            5: self.__entry5,
+            6: self.__entry6,
+        }
+        if plant_found:
+            entries[counter].configure(bg="green yellow")
+        else:
+            entries[counter].configure(bg="firebrick")
 
     def calculate_save(self, plantlist):
         if len(plantlist) != 0:
@@ -331,18 +358,18 @@ class Window:
 
     def assign_random_attribute(self):
         random_attribute = random.randint(0, (len(self.__ATTRIBUTES) - 1))
-        additional_string = ""
         additional_list = self.__ATTRIBUTES[random_attribute]["Additional"].split(", ")
-        if len(additional_list) > 0:
+        additional_string = ""
+        if len(additional_list) > 1:
             random_value = random.randint(0, (len(additional_list) - 1))
             additional_string = " (" + str(additional_list[random_value]) + ")"
-        self.__random_attribute.configure(text=str(self.__ATTRIBUTES[random_attribute]["Effect"]) + additional_string)
+        self.__random_attribute.configure(text=str(self.__ATTRIBUTES[random_attribute]["Attribute"]) + additional_string)
 
     def assign_random_effect(self):
         random_effect = random.randint(0, (len(self.__EFFECTS) - 1))
         additional_string = ""
         additional_list = self.__EFFECTS[random_effect]["Additional"].split(", ")
-        if len(additional_list) > 0:
+        if len(additional_list) > 1:
             random_value = random.randint(0, (len(additional_list) - 1))
             additional_string = " (" + str(additional_list[random_value]) + ")"
         self.__random_effect.configure(text=str(self.__EFFECTS[random_effect]["Effect"]) + additional_string)
@@ -358,7 +385,8 @@ class Window:
                 plants_in_biome.append(plant["Name"])
         randomly_chosen_plant = plants_in_biome[random.randint(0, (len(plants_in_biome) - 1))]
         random_portion = random.choices(PORTIONS, weights=(10, 6, 3, 1), k=1)
-        self.__random_plant.configure(text=str(string.capwords(randomly_chosen_plant)) + ", " + str(random_portion) + " Portions")
+        self.__random_plant\
+            .configure(text=str(string.capwords(randomly_chosen_plant)) + ", " + str(random_portion) + " Portions")
 
 
 def main():
